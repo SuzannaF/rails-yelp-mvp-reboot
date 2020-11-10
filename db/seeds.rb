9fe52f4d0f432eb3ec6f32c9227cdc1a92722126
Suzanna Filipowska <suzanna.filipowska@gmail.com>
2020-11-10 18:22:03 +0100

    repetition of advanced routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+puts "Gonna create some restaurants hihi"
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Company.duns_number,
+    phone_number: Faker::Address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Created restaurant #{restaurant.id}"
+end
